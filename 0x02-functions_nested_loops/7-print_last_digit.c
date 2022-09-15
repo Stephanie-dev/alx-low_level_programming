f9989fb974bdce37f795bd7d42186bc46a890ac1
Stephanie <stephaniechiamaka63@gmail.com>
2022-09-14 18:12:50 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,8 +6,11 @@
  */
 int print_last_digit(int r)
 {
+int n;
 if (r < 0)
-r *= -1;
-_putchar('0' + (r % 10));
-return (r % 10);
+n = -1 * (r % 10);
+else
+n = r % 10;
+_putchar((n % 10) + '0');
+return (n % 10);
 }
