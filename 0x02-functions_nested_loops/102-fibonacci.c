b3faab4b92a0998ec6e0924c321e34fcd954344d
Stephanie <stephaniechiamaka63@gmail.com>
2022-09-14 19:37:08 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -12,6 +12,7 @@ fibonacci[0] = 1;
 fibonacci[1] = 2;
 printf("%ld, %ld, ", fibonacci[0], fibonacci[1]);
 for (i = 2; i < 50; i++)
+{
 fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
 if (i == 49)
 printf("%ld\n", fibonacci[i]);
