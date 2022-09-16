8afd751c4463154b2e7638d22faf49f25db3ee95
Stephanie <stephaniechiamaka63@gmail.com>
2022-09-16 07:43:46 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,7 @@
-#ifndef MAINN_H
+#ifndef MAIN_H
 #define MAIN_H
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
-void print_most_numbers(void);
+void print_numbers(void);
 #endif
